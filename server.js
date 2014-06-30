ee1d49cfda1fca227c887b298d63201f678b49a8
Roman Zhak <zhak55@ya.ru>
2014-06-30 22:43:21 +0400

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ http.createServer(function (req, res) {
 	
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write("Welcome to Node.js on OpenShift!\n\n");
-  res.write("From your git!\n\n");
+  res.write("From your git 0.1.0!\n\n");
   res.end("Your IP address seems to be " + addr + "\n");
 }).listen(port, ipaddr);
 console.log("Server running at http://" + ipaddr + ":" + port + "/");
